570a6db3e73d4bdb722e4ef3ee9066c20b8b62c8
Tomasz <korniszuk.tomasz@gmail.com>
2023-12-08 20:34:23 +0100

    all

--- skrypt1.py
+++ skrypt1.py
@@ -1,8 +1,12 @@
 from pydub import AudioSegment
 import os
+from pathlib import Path
 
-source_path = "C:\\Users\\korni\\seminarium\\semi\\source"
-dest_path = "C:\\Users\\korni\\seminarium\\semi\\exported"
+AudioSegment.converter = "C:\\Users\\korni\\Downloads\\ffmpeg-2023-11-02-git-4dbfb52230-full_build\\bin\\ffmpeg.exe"
+AudioSegment.ffmpeg = "C:\\Users\\korni\\Downloads\\ffmpeg-2023-11-02-git-4dbfb52230-full_build\\bin\\ffmpeg.exe"
+AudioSegment.ffprobe = "C:\\Users\\korni\\Downloads\\ffmpeg-2023-11-02-git-4dbfb52230-full_build\\bin\\ffprobe.exe"
+source_path = r"C:\\Users\\korni\\seminarium\\semi\\source"
+dest_path = r"C:\\Users\\korni\\seminarium\\semi\\exported"
 
 for file in os.listdir(source_path):
     music_type = os.listdir(os.path.join(source_path , file))
@@ -12,10 +16,11 @@ for file in os.listdir(source_path):
         pass
 
     for music in music_type:
-        # mp3 = os.path.join(source_path + "\\" + file,  music)
-        mp3 = source_path + "\\"+file+"\\"+music
+        mp3 = os.path.join(source_path + "\\" + file,  music)
+        # mp3 = source_path + "\\"+file+"\\"+music
         print(mp3)
         audio = AudioSegment.from_mp3(mp3)
         export = dest_path+ "\\" + file + "\\" + music
         audio.export(export , format = "mp3")
-print("Udało się ")
+
+
--- skrypt2.py
+++ skrypt2.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-source_path = "D:\\semi\\exported"
-dest_path = "D:\\semi\\spektrogramy"
-
-for file in os.listdir(source_path):
+source_path = "C:\\Users\\korni\\seminarium\\semi\\exported"
+dest_path = "C:\\Users\\korni\\seminarium\\semi\\data"
+list = ['latino', 'rock']
+# for file in os.listdir(source_path):
+for file in list:
     music_type = os.listdir(os.path.join(source_path , file))
     try:
         os.mkdir(os.path.join(dest_path , file))
@@ -16,19 +17,24 @@ for file in os.listdir(source_path):
     for music in music_type:
         music_name = music.split('.')[0]
         plik_docelowy = os.path.join(source_path + '\\' + file , music)
-        print(file)
-        print(plik_docelowy)
+        plik_dest = dest_path + '\\' + file +"\\"+ music_name + ".csv"
+        try:
 
-        y , sr = librosa.load(plik_docelowy)  #
+            if not os.path.exists(os.path.join(dest_path + '\\' + file , music_name + ".csv")):
+                y , sr = librosa.load(plik_docelowy)  #
+                librosa.feature.melspectrogram(y = y , sr = sr)
 
-        librosa.feature.melspectrogram(y = y , sr = sr)
+                S = librosa.feature.melspectrogram(y = y , sr = sr , n_mels = 128 , fmax = 12000)
+                S_dB = librosa.power_to_db(S , ref = np.max)
+                librosa.display.specshow(S_dB , x_axis = 'time' , y_axis = 'mel' , sr = sr , fmax = 12000)
+                newS_DB = S_dB[:,:2375]
+                np.savetxt(plik_dest, newS_DB,delimiter=",")
 
-        S = librosa.feature.melspectrogram(y = y , sr = sr , n_mels = 128 , fmax = 12000)
-        S_dB = librosa.power_to_db(S , ref = np.max)
-        librosa.display.specshow(S_dB , x_axis = 'time' , y_axis = 'mel' , sr = sr , fmax = 12000)
+        except EOFError:
+            print(1)
 
-        plt.colorbar(format = '%+2.0f dB')
-        plt.title('Mel-spektrogram')
-        plt.tight_layout()
-        plt.savefig(os.path.join(dest_path + '//' + file , music_name + '.png'))
-        plt.clf()
+        # plt.colorbar(format = '%+2.0f dB')
+        # plt.title('Mel-spektrogram')
+        # plt.tight_layout()
+        # plt.savefig(os.path.join(dest_path + '//' + file , music_name + '.png'))
+        # plt.clf()
